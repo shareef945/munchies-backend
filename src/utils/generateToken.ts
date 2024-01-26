a81368e8fcb109dc1ed6fe225c632ea7bce659a0
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-01-26 21:42:42 +0000

    minor

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,5 +1,4 @@
 import jwt from "jsonwebtoken";
-//
  
 export function generateToken(password: string, userId: string, client: string) {
   return jwt.sign({ userId, client }, password, { expiresIn: "1h" });
