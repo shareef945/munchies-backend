8f337834773fd24a63c88bdbe3d85663ac386e41
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-03-05 09:23:21 +0000

    bug: minor updates to console logs

--- src/scheduled-jobs/hubtel.ts
+++ src/scheduled-jobs/hubtel.ts
@@ -4,23 +4,10 @@ import { downloadCsvFromSftp, moveFilesFromSftp } from "../utils/utils";
 
 var cron = require("node-cron");
 
-// Flag to indicate whether the job is currently running
-// let isJobRunning = false;
-
-export function scheduleHubtelJob() {
+export async function scheduleHubtelJob() {
   // cron.schedule("11 16 * * *", async () => {
-    cron.schedule(`0 ${HOUR_OF_THE_DAY} * * *`, async () => {
-
-    // Check if the job is already running
-    // if (isJobRunning) {
-    //   console.log(
-    //     `Skipping this run because the previous job is still running at ${new Date().toLocaleString()}`
-    //   );
-    //   return; // Exit the function if the job is running
-    // }
-
+  cron.schedule(`0 ${HOUR_OF_THE_DAY} * * *`, async () => {
     console.log(`Running the HUBTEL job at ${new Date().toLocaleString()}`);
-    // isJobRunning = true; // Set the flag to indicate the job is running
 
     try {
       const csvData = await downloadCsvFromSftp();
@@ -28,14 +15,14 @@ export function scheduleHubtelJob() {
     } catch (error) {
       console.error("Failed to complete the cron job:", error);
     } finally {
-      // isJobRunning = false; // Reset the flag when the job is finished
     }
   });
 
   cron.schedule("0 9 * * *", async () => {
-    console.log("Moving files..");
+    console.log(`Moving files at${new Date().toLocaleString()}`);
     try {
       await moveFilesFromSftp();
+      console.log(`Successfully moved files at ${new Date().toLocaleString()}`);
     } catch (err: any) {
       console.log("Error moving files ..", err);
     }
